4e243898a8df3261f5373ddbbc59656970ec9004
x4lva <30348460+Dimonchak@users.noreply.github.com>
2021-02-03 20:08:48 +0200

    Task 1

---
+++ main.php
@@ -0,0 +1,24 @@
+<?php
+
+
+function MinMax(array $a){
+    return max($a) - min($a);
+}
+
+// Function using recursion
+function Sum($n){
+
+    if ($n==0){
+        return 0;
+    }
+
+    return $n+Sum($n-1);
+}
+
+echo MinMax([1,4,5,3,17]);
+
+echo Sum(3);
+
+
+
+?>
